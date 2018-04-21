e147e87794d5585d11dfccbe1b57edc2e4ebb947
陈淇格 <cqg186495@outlook.com>
2018-04-21 15:40:21 +0800

    initial commit

---
+++ src/components/CommonHeader.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class = "header">
+    <div class = "header-logo">
+      <div class = "header-button-container">
+
+      </div>
+      <div class = "header-user">
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'CommonHeader',
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .header{
+    width: 100%;
+    height: 10rem;
+    position: fixed;
+    left: 0;
+    top:0;
+    background-color: rgb(210,224,233);
+  }
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class = "container">
+    <common-header>
+
+    </common-header>
+  </div>
+</template>
+
+<script>
+  import CommonHeader from '../components/CommonHeader'
+
+  export default {
+    name: 'NewShow',
+    data () {
+      return {
+
+      }
+    },
+    components:{
+    	CommonHeader
+    },
+    created(){
+
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .container{
+
+  }
+
+</style>
